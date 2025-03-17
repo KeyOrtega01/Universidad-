3d865b60e7d6ed8165ba813beef1e78a478b6599
KeyOrtega01 <Keidysortega25@gmail.com>
2025-03-17 10:13:10 -0500

    Initial commit

---
+++ Api/personas.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+#Ruta que responde a la solicitud GET del endpoint para devolver una lista de personas 
+@app.route('/api/personas', methods= ['GET'])
+def index():
+    #Lista de personas que devuelve el endpoint en formato JSON 
+    lista_personas = [
+        {'name': 'Keidys Salguedo', 'age': 20},
+        {'name': 'Rafael Mantilla', 'age': 21},
+        {'name': 'Margarita Ortega', 'age': 24},
+        {'name': 'Carlos Monsalve', 'age': 30}
+    ]
+    #Convierte la lista en un JSON y lo devuelve con un codigo 200 que se refiere al metodo GET
+    return jsonify(lista_personas), 200
+
+if __name__ == '__main__':
+    #Inicializa la aplicacion con un debug activado
+    app.run(debug= True)
